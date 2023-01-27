6ea5e6af3fdca64847e50ec12c00ed6422d333fd
preciousoreva <preciousakporeha@gmail.com>
2023-01-26 22:50:49 -0500

    Add a binary search function

---
+++ binary-search.php
@@ -0,0 +1,16 @@
+function binary_search($arr, $x) { 
+    $left = 0; 
+    $right = count($arr) - 1; 
+    while ($left <= $right) { 
+        $mid = floor(($left + $right) / 2); 
+        if ($arr[$mid] == $x) { 
+            return $mid; 
+        } 
+        if ($arr[$mid] < $x) { 
+            $left = $mid + 1; 
+        } else { 
+            $right = $mid - 1; 
+        } 
+    } 
+    return -1; 
+} 
